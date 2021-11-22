98c91709eae983e271e7a1c734dc7dc697fdf059
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2021-11-22 08:36:31 +0100

    updated

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,12 +1,20 @@
 import cv2
 import mediapipe as mp
-import time
+import time, math
 
 class handDetector():
-    def __init__(self):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands()
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, 1, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+
+
+        self.tipIds = [4, 8, 12, 16, 20]
 
     def getFps(self, previous_time):
         current_time = time.time()
@@ -31,6 +39,7 @@ class handDetector():
         yList = []
         bbox = []
         self.lmList = []
+
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
@@ -40,14 +49,51 @@ class handDetector():
                 yList.append(cy)
                 self.lmList.append([id, cx, cy])
 
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                bbox = xmin, ymin, xmax, ymax
+
                 if draw:
+                    cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                                  (0, 255, 0), 2)
+
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
         return self.lmList , bbox
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Fingers
+        for id in range(1, 5):
+
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 """
 Hand Tracing Module
 MurtazasWorkshopRoboticsandAI
 """
-
-
